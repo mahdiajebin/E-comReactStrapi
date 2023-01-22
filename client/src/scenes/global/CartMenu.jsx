3719614cfe7f20c602a65bfe3a85f5e4f851efc2
mahdiajebin <mahdiajebin@gmail.com>
2023-01-21 22:28:43 -0500

    firstcom

---
+++ client/src/scenes/global/CartMenu.jsx
@@ -0,0 +1,7 @@
+import{Box, Button, Divider, IconButton, Typography} from "@mui/material";
+import {useSelector, useDispatch, } from 'react-redux';
+import CloseIcon from '@mui/icons-material/Close';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import styled from '@emotion/styled';
+import {shades} from "../../theme";
